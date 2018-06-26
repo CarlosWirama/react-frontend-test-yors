06ac3df19fa52b12e643e7446c9283cc75993213
CarlosWirama <vincentius_carlos@yahoo.co.id>
2018-06-26 07:32:39 +0700

    installing react-native-vector-icons, implement raw bottom-nav from react-navigation

--- ignite/DevScreens/PresentationScreen.js
+++ ignite/DevScreens/PresentationScreen.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { ScrollView, Text, Image, View, TouchableOpacity } from 'react-native'
+import { Platform, ScrollView, Text, Image, View, TouchableOpacity } from 'react-native'
 import { Images } from './DevTheme'
 import ButtonBox from './ButtonBox'
-import { StackNavigator } from 'react-navigation'
+import { StackNavigator, TabNavigator, TabBarBottom } from 'react-navigation'
+import Icons from 'react-native-vector-icons/Ionicons';
+
 // Screens
 import APITestingScreen from './APITestingScreen'
 import ComponentExamplesScreen from './ComponentExamplesScreen'
@@ -81,7 +83,105 @@ class PresentationScreen extends React.Component {
   }
 }
 
+const HomeTab = TabNavigator(
+  {
+    Home: { screen: PresentationScreen },
+    APITestingScreen: { screen: APITestingScreen },
+    ComponentExamplesScreen: { screen: ComponentExamplesScreen },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: '#fff',
+        ...Platform.select({
+          ios: {
+            borderBottomColor: "#ececec",
+            borderBottomWidth: 1,
+            height: 51
+          },
+          android: {
+            backgroundColor: 'cyan',
+            elevation: 0,
+            borderBottomColor: "#ececec",
+            borderBottomWidth: 1,
+            height: 20,
+            marginTop: 0
+          },
+        }),
+      },
+      // tabBarLabel: () => {
+      //   const { routeName } = navigation.state;
+      //   switch (routeName) {
+      //     case 'Explore':
+      //       return 'Jelajah';
+      //     case 'MyBooking':
+      //       return 'Pesananku';
+      //     case 'Favorit':
+      //       return 'Favorit';
+      //     case 'MessageBlank':
+      //       return 'Inbox';
+      //     case 'AccountPage':
+      //       return 'Akun';
+      //   }
+      // },
+      tabBarIcon: ({ focused }) => {
+        const { routeName } = navigation.state;
+        let iconName, control;
+        switch (routeName) {
+          case 'Home':
+            iconName = `ios-home${focused ? '' : '-outline'}`;
+            break;
+          case 'APITestingScreen':
+            iconName = `ios-paper${focused ? '' : '-outline'}`;
+            break;
+          case 'ComponentExamplesScreen':
+            iconName = `ios-search${focused ? '' : '-outline'}`;
+            // break;
+          // case 'MessageBlank':
+          //   iconName = `ios-mail-open${focused ? '' : '-outline'}`;
+          //   break;
+          // case 'AccountPage':
+          //   iconName = `ios-person${focused ? '' : '-outline'}`;
+        }
+        return (
+          <View>
+            <Icons
+              name={iconName}
+              size={28}
+              style={{ marginBottom: 0, }}
+              // color={focused ? Colors.bottomTabSelected : Colors.bottomTabBlurred}
+            />
+          </View>
+        );
+      },
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    swipeEnabled: true,
+    lazy: false,
+
+    tabBarOptions: {
+      // activeTintColor: Colors.bottomTabSelected,
+      labelStyle: {
+        fontSize: 12,
+        // fontFamily: 'Hind-SemiBold',
+        // ...Platform.select({
+        //   ios: {
+        //     marginBottom: -8,
+        //     marginTop:-8
+        //   },
+        //   android: {
+        //     lineHeight: 18,
+        //   },
+        // }),
+      },
+      style: { paddingBottom: 5, height: 60, backgroundColor: '#fbfbfb', borderTopColor: '#ececec', }
+    },
+  }
+);
 export default StackNavigator({
+  HomeTab: {screen: HomeTab},
   PresentationScreen: {screen: PresentationScreen},
   APITestingScreen: {screen: APITestingScreen},
   ComponentExamplesScreen: {screen: ComponentExamplesScreen},
@@ -94,13 +194,15 @@ export default StackNavigator({
     opacity: 1,
     backgroundColor: '#3e243f'
   },
-  initialRouteName: 'PresentationScreen',
+  initialRouteName: 'HomeTab',
   headerMode: 'none',
   // Keeping this here for future when we can make
   navigationOptions: {
     header: {
       left: (
-        <TouchableOpacity onPress={() => window.alert('pop')} ><Image source={Images.closeButton} style={{marginHorizontal: 10}} /></TouchableOpacity>
+        <TouchableOpacity onPress={() => window.alert('pop')} >
+          <Image source={Images.closeButton} style={{marginHorizontal: 10}} />
+        </TouchableOpacity>
       ),
       style: {
         backgroundColor: '#3e243f'
@@ -108,3 +210,4 @@ export default StackNavigator({
     }
   }
 })
+
